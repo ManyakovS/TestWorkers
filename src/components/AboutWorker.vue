b1d68e3140e84fb78681566340e14f81eaee6245
Rafael2111 <117172668+Rafael2111@users.noreply.github.com>
2023-06-29 10:52:36 +0700

    feat: Карточка работника

--- src/components/AboutWorker.vue
+++ src/components/AboutWorker.vue
@@ -1,20 +1,90 @@
 <template>
     <div class="about__container" @click="close">
 
-        <div class="about__information">
-            agsdgsgsdggd
+        <div v-if="isLoading" class="about__information">
+            <h2 class="about__information__title">{{ information.name }}</h2>
+
+            <div class="information__elements">
+
+                <img class="information__elements__image" :src="information.image" alt="">
+
+                <ul class="information__elements__list">
+                    <li class="list__element">
+                        Логин: {{ information.login }}
+                    </li>
+
+                    <li class="list__element">
+                        Электронная почта: {{ information.email }}
+                    </li>
+
+                    <li class="list__element">
+                        Отдел: {{ information.worker.department }}
+                    </li>
+
+                    <li class="list__element">
+                        Должность: {{ information.worker.position }}
+                    </li>
+
+                    <li class="list__element">
+                        Зачислен: {{ information.worker.adopted_at }}
+                    </li>
+                </ul>
+            </div>
+            <div class="information__elements__about">
+                <h3 >
+                    О себе:
+                </h3>
+                <p class="info-text">
+                    {{ information.about }}
+                </p>
+            </div>
+
+
         </div>
 
     </div>
 </template>
 
 <script setup lang="ts">
+import { onMounted, onUnmounted, Ref, ref } from 'vue'
+import { useRoute } from 'vue-router';
 import router from '@/router';
+import { useAxios } from '@vueuse/integrations/useAxios'
+
+const information: any = ref()
+const isLoading: Ref<boolean> = ref(false)
+
+onMounted(async () => {
+    const route = useRoute();
+    information.value = await getInformationAboutWorker(route.params.id.toString())
+    isLoading.value = true;
+
+    document.body.style.overflowY = "hidden";
+})
+
+onUnmounted(() => {
+    document.body.style.overflowY = "auto";
+})
+
+const getInformationAboutWorker = async (id: number | string) => {
+    try {
+        const { data } = await useAxios(`https://test.atwinta.online/api/v1/workers/${id}`,
+            {
+                method: 'GET',
+                headers: {
+                    'Authorization': 'Bearer ' + localStorage.getItem('auth_token')
+                }
+            })
+        return data.value
+    } catch (error) {
+
+        return 500
+    }
+}
 
 const close = () => {
     router.push('/workers')
 }
-
 </script>
 
 <style scoped lang="scss">
@@ -28,13 +98,42 @@ const close = () => {
 }
 
 .about__information {
-    width: 50vw;
-    height: 60vh;
+    width: 60vw;
+    height: 70vh;
     position: absolute;
     background-color: #fff;
-    top: calc(50% - 60vh / 2);
-    left: calc(50% - 50vw / 2);
+    top: calc(50% - 70vh / 2);
+    left: calc(50% - 60vw / 2);
     border-radius: 20px;
-}
+    padding: 2%;
 
+    &__title {
+        text-align: center;
+    }
+
+    .information__elements {
+        margin-top: 4%;
+        display: grid;
+        grid-template-columns: 1.2fr 2fr;
+        gap: 10px;
+
+        &__image {
+            width: clamp(250px, 100%, 350px);
+            height: clamp(250px, 100%, 350px);
+        }
+
+        &__list {
+            .list__element {
+                font-size: 1.5rem;
+                font-weight: 500;
+                margin-bottom: 1.5vh;
+            }
+        }
+
+        &__about {
+            margin-top: 1vh;
+        }
+
+    }
+}
 </style>
